412c39573965f739eb17add0ad98af1a8b39bd33
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-03 19:06:55 -0300

    feat: add banco de dados

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
-#Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
+db= SQLAlchemy(app)
+
+
+class Prroduct(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
 @app.route('/')
 def hello_wold():
     return 'Hello World'
